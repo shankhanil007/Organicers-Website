9750dc93f1594852c8a910ba046966661bb46395
Anubhav12345678 <45006874+Anubhav12345678@users.noreply.github.com>
2020-07-06 16:13:41 +0530

    Update main.js

--- testimonial/main.js
+++ testimonial/main.js
@@ -3,17 +3,15 @@
 var x = document.getElementById("mypanel");
 var pgsz=3;
 var pgno=1;
-var obj = {
-  title: "CATEGORY",
-  desc: "DESCRIPTION"
-};
+
 
 var ourRequest = new XMLHttpRequest();
-ourRequest.open("GET","http://35.184.138.248:8000/image/?page="+pgno+"&page_size="+pgsz);
+ourRequest.open("GET","http://35.184.138.248.xip.io:8000/testmonial/?format=json&page="+pgno+"&page_size="+pgsz);
 ourRequest.onload = function(){
   if(ourRequest.status>=200 && ourRequest.status<400)
   {
      // alert("conn established");
+     console.log(ourRequest);
      var data = JSON.parse(ourRequest.responseText);
      cnt = data.count;
      renderhtml(data,pgno,pgsz);
@@ -36,32 +34,23 @@ function renderhtml(data,pagenumber,pagesize)
     var rem = cnt-(pagenumber-1)*pagesize;
     for(i=0;i<Math.min(pagesize,rem);i++)
     {
-       res+="<p style='font-size:2.5rem;'>"+ obj.title +"</p>";
-       res+="<p style='font-size:1rem;'>"+  obj.desc +"</p>";
-       res+="<div class='row'>";
-       
-       console.log(data["results"].length);
-         for(j=0; j<data["results"].length;j++)
-         {
-          res+="<div style='height:17rem;'class='col-lg-3 col-md-6'>";
-          // console.log(data["results"][j].img);
-          res+="<img style='width:100%;height:100%' src='" + data["results"][j].img + "' >";
-          res+="</div>";
-         }
-         //if images are less than 4
-         for(j=0;j<4-data["results"].length;j++)
-         {
-          res+="<div style='height:17rem;' class='col-lg-3 col-md-6'>"
-          res+="<img style='width:100%;height:100%' src='" + data["results"][0].img + "' >";
-          res+="</div>";
-         }
-
-       res+="</div>";
+      res+="<p style='font-size:5em;'>"+ data["results"][i].client_name +"</p>";
+      res+="<p style='font-size:2em'>"+ data["results"][i].description +"</p>";
+      res+="<div class='row'>";
+      for(j=0;j<Math.min(4,data["results"][i].image.length); j++)
+      {
+        res+="<div class='col-lg-3 col-md-4 cateimg' style='overflow: hidden'>";
+         res+="<img src='"+ data["results"][i].image[j].img +"'>";
+        res+="</div>";
+      }
+      res+="</div>";
+      res+="<br><hr class='style-seven'><br>";
     }
+  res+="</div>";
 
 
     res+="<br><br>";
-    res+="<div class='row'><div style='margin: auto;'class='col-6'>"
+    res+="<center><div class='row'><div style='margin: auto;'class='col-6'>"
     for( i=0;i<Math.ceil(cnt/pagesize);i++)
     {
       if((i+1)==pagenumber)
@@ -70,18 +59,19 @@ function renderhtml(data,pagenumber,pagesize)
         res+="<button class='btn btn-primary bslide' style='font-size:0.7rem' value='"+ (i+1) +"' id = '"+ (i+1) +"' onclick='req("+(i+1)+","+pagesize+")'>"+ (i+1) +"</button>";
     }
     res+="</div></div>"
-    res+="</div>";
+    res+="</div></center>";
     x.innerHTML = res;
 }
 
 function req(pagenumber,pagesize)
 {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open("GET","http://35.184.138.248:8000/image/?page="+pagenumber+"&page_size="+pagesize);
+    ourRequest.open("GET","http://35.184.138.248.xip.io:8000/testmonial/?format=json&page="+pagenumber+"&page_size="+pagesize);
     ourRequest.onload = function(){
       if(ourRequest.status>=200 && ourRequest.status<400)
       {
        // alert("conn established");
+       console.log(ourRequest);
        var data = JSON.parse(ourRequest.responseText);
        var cnt = data.count;
        renderhtml(data,pagenumber,pagesize);
